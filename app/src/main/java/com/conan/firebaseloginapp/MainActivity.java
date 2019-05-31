c7eb3ce40b40544a7422ed032406e6e3f74a0b61
Hossam <conancode96@gmail.com>
2019-05-31 21:57:00 +0200

    added main page signout button/logic

--- app/src/main/java/com/conan/firebaseloginapp/MainActivity.java
+++ app/src/main/java/com/conan/firebaseloginapp/MainActivity.java
@@ -1,13 +1,53 @@
 package com.conan.firebaseloginapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button signOut_btn;
+    private FirebaseAuth auth;
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_page);
+
+        signOut_btn = (Button) findViewById(R.id.signOut_btn);
+
+        auth = FirebaseAuth.getInstance();
+
+        // this listener will be called when there is change in firebase user session
+        FirebaseAuth.AuthStateListener authListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user == null) {
+                    // user auth state is changed - user is null
+                    // launch login activity
+                    startActivity(new Intent(MainActivity.this, LoginActivity.class));
+                    finish();
+                }
+            }
+        };
+
+        signOut_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                auth.signOut();
+            }
+        });
+
+
     }
+
 }
